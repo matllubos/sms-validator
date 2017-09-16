9c4f3b8613e17989e2a10ffd0dc92b5bfb56a30f
Lubos Matl <matllubos@gmail.com>
2017-09-17 01:00:19 +0200

    Improved settings and Token manager

--- sms_validator/config.py
+++ sms_validator/config.py
@@ -1,3 +1,20 @@
-from django.conf import settings
+from django.conf import settings as django_settings
 
-SMS_MAX_TOKEN_AGE = getattr(settings, 'MAX_SMS_TOKEN_AGE', 60 * 60)
+
+DEFAULTS = {
+    'MAX_TOKEN_AGE_SECONDS': 60 * 60,
+    'TOKEN_LENGTH': 6,
+    'UNIVERSAL_TOKEN': None,
+}
+
+
+class Settings(object):
+
+    def __getattr__(self, attr):
+        if attr not in DEFAULTS:
+            raise AttributeError('Invalid SMS validator setting: "{}"'.format(attr))
+
+        return getattr(django_settings, 'SMS_VALIDATOR_{}'.format(attr), DEFAULTS[attr])
+
+
+settings = Settings()
